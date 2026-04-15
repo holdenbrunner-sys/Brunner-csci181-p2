e56abcbc6970665bce73056d9e5a6b89ffd71785
holdenbrunner-sys <holdenbrunner@gmail.com>
2026-04-15 17:37:11 -0600

    Java

---
+++ modal.js
@@ -0,0 +1,45 @@
+//ARRAY
+const projects = [
+  {
+    title: "Index",
+    details: "details",
+    link_text: "View in Github",
+    href: "https://github.com/holdenbrunner-sys/Brunner-csci181-practice/blob/main/index.html",
+  },
+  {
+    title: "Resume",
+    details: "details",
+    link_text: "View in Github",
+    href: "https://github.com/holdenbrunner-sys/Brunner-csci181-practice/blob/main/resume.html",
+  },
+  {
+    title: "Bento Grid",
+    details: "details",
+    link_text: "View in Github",
+    href: "https://github.com/holdenbrunner-sys/Brunner-csci181-practice/blob/main/bento-grid.html",
+  },
+  {
+    title: "Weather",
+    details: "details",
+    link_text: "View in Github",
+    href: "https://github.com/holdenbrunner-sys/Brunner-csci181-practice/blob/main/weather.html",
+  },
+  {
+    title: "Weather Script",
+    details: "details",
+    link_text: "View in Github",
+    href: "https://github.com/holdenbrunner-sys/Brunner-csci181-practice/blob/main/scripts/weather.js",
+  },
+  {
+    title: "Project 1",
+    details: "details",
+    link_text: "View in Github",
+    href: "https://github.com/holdenbrunner-sys/brunner-csci181-p1",
+  },
+];
+//Modal Elements
+modal = document.querySelectorAll("li");
+modal_title = document.querySelectorAll("h3");
+
+console.log(modal);
+console.log(modal_title);
